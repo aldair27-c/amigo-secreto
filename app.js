8c44dee06afb28de78a0e5e68e805328329f1b44
aldair27-c <aldaircorrea27@gmail.com>
2025-09-19 14:41:44 -0500

    Array para almacenar los nombres

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Lista donde guardaremos los nombre
+let amigos = [];
